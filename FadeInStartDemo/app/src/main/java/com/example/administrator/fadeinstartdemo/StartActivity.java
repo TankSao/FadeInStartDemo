9674199c5d03181183c6266c5890ff56c19205ad
TankSao <38127423+TankSao@users.noreply.github.com>
2018-10-20 14:00:28 +0800

    2018-10-20

---
+++ FadeInStartDemo/app/src/main/java/com/example/administrator/fadeinstartdemo/StartActivity.java
@@ -0,0 +1,57 @@
+package com.example.administrator.fadeinstartdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 2018/10/20.
+ */
+
+public class StartActivity extends AppCompatActivity{
+    private ImageView iv_start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        initImage();
+    }
+
+    private void initImage() {
+        iv_start = (ImageView) findViewById(R.id.start);
+        //进行缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1.0f, 1.4f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(4000);
+        //动画播放完成后保持形状
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //可以在这里先进行某些操作
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        iv_start.startAnimation(scaleAnimation);
+    }
+
+    private void startActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+}
